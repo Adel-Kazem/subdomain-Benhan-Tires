d0b44afd8f5074e5344907603102c9c0ed8abec8
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-03-27 15:20:49 +0200

    Delete product

--- products.js
+++ products.js
@@ -26,31 +26,6 @@ const PRODUCTS = [
     ]
   },
   {
-    id: 3,
-    name: "All-Terrain Off-Road Wheels",
-    description: "Conquer any terrain with our rugged all-terrain wheels. Built to withstand the toughest off-road conditions, these wheels offer superior traction and durability for your adventures beyond the pavement.",
-    price: 379.99,
-    rating: 4.6,
-    stock: 25,
-    images: [
-      "https://images.unsplash.com/photo-1611821064684-0ef5eed64c8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      "https://images.unsplash.com/photo-1611821064897-8ac7d2c9be64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      "https://images.unsplash.com/photo-1611821065763-5ea0aad59df3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
-    ],
-    colors: [
-      "Bronze",
-      "Black",
-      "Graphite"
-    ],
-    features: [
-      "Reinforced construction",
-      "Deep lip for better traction",
-      "Mud and rock resistant",
-      "Sizes from 16\" to 20\"",
-      "Compatible with most off-road tires"
-    ]
-  },
-  {
     id: 4,
     name: "All-Terrain Off-Road Wheels",
     description: "Conquer any terrain with our rugged all-terrain wheels. Built to withstand the toughest off-road conditions, these wheels offer superior traction and durability for your adventures beyond the pavement.",
